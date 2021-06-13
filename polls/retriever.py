18794dc35727b18d762e9eff0d20aebd5bac1f1a
zbala-hcttp <zeynepbala@cs.hacettepe.edu.tr>
2021-06-13 20:51:13 +0300

    commit

--- polls/retriever.py
+++ polls/retriever.py
@@ -7,9 +7,9 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.analysis.core import WhitespaceAnalyzer, SimpleAnalyzer
 from org.apache.lucene.analysis.en import EnglishAnalyzer
-from org.apache.lucene.search import TermQuery, IndexSearcher
+from org.apache.lucene.search import TermQuery, IndexSearcher, BooleanClause
 from org.apache.lucene.index import Term, DirectoryReader
-from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.queryparser.classic import QueryParser, MultiFieldQueryParser
 
 
 class Retriever:
@@ -22,20 +22,43 @@ class Retriever:
 			self.vm = lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 			self.vm.attachCurrentThread()
 
-	def retrieve(self):
-		ind_dir = self.INDEX_DIR + "SA/"
+		self.SHOULD = BooleanClause.Occur.SHOULD
+
+	def retrieve(self, fields_, query_, analyzer_):
 		self.vm.attachCurrentThread()
+
+		# Create a new retrieving analyzer
+		if analyzer_ == "opt1":
+			ind_dir = self.INDEX_DIR + "SIMPA/"
+			analyzer = SimpleAnalyzer()
+		elif analyzer_ == "opt2":
+			analyzer = WhitespaceAnalyzer()
+			ind_dir = self.INDEX_DIR + "WA/"
+		elif analyzer_ == "opt3":
+			analyzer = EnglishAnalyzer()
+			ind_dir = self.INDEX_DIR + "EA/"
+		else:
+			analyzer = StandardAnalyzer()
+			ind_dir = self.INDEX_DIR + "SA/"
+
 		store = SimpleFSDirectory(Paths.get(ind_dir))
 		searcher = IndexSearcher(DirectoryReader.open(store))
 
-		# Create a new retrieving analyzer
-		analyzer = StandardAnalyzer()
-		query = QueryParser("directions", analyzer).parse("mix in onion")
-		topDocs = searcher.search(query, 200).scoreDocs
+		fields = []
+		sh = []
+		for keys in fields_.keys():
+			if fields_[keys]:
+				fields.append(keys)
+				sh.append(self.SHOULD)
+
+		if query_ == None or query_ == "":
+			query_ = "abcdef"
+
+		query = query = MultiFieldQueryParser.parse(query_, fields, sh, analyzer)
+		topDocs = searcher.search(query, 50).scoreDocs
 		docs = []
 		for topDoc in topDocs:
 		    doc = searcher.doc(topDoc.doc)
-		    #print(doc.get("link"))
 		    docs.append({"link": doc.get("link"),
 		    			 "category": doc.get("category"),
 		    			 "name": doc.get("name"),
--- polls/views.py
+++ polls/views.py
@@ -2,33 +2,47 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import retriever
 
-"""
-results = [
-	{
-		"category": "breakfast",
-		"name": "egg",
-		"ingredients": "1 egg, half cup of milk",
-		"directions": "bake egg, add milk"
-	},
-	{
-		"category": "soup",
-		"name": "tomato soup",
-		"ingredients": "1 tomato, water",
-		"directions": "slice tomato, add water"
-	}
-]
-"""
+
+fields = {
+	"category" : False,
+	"name" : False,
+	"ingredients" : False,
+	"directions" : False
+}
 
 retriever_ = retriever.Retriever()
 
 def home(request):
-	recipes= retriever_.retrieve()
+	if "category" in request.GET:
+		fields["category"] = True
+	else:
+		fields["category"] = False
+	if "name" in request.GET:
+		fields["name"] = True
+	else:
+		fields["name"] = False
+	if "ingredients" in request.GET:
+		fields["ingredients"] = True
+	else:
+		fields["ingredients"] = False
+	if "directions" in request.GET:
+		fields["directions"] = True
+	else:
+		fields["directions"] = False
+
+	url_string = "?" + request.GET.urlencode()
+	if not "page" in request.GET:
+		url_string += "&page=1"
+	query = request.GET.get("query")
+	analyzer = request.GET.get("analyzer")
+
+	recipes= retriever_.retrieve(fields, query, analyzer)
 	page = request.GET.get('page', 1)
-	paginator_ = Paginator(recipes, 10)
+	paginator_ = Paginator(recipes, 5)
 	try:
 		results = paginator_.page(page)
 	except EPageNotAnInteger:
 		results = paginator_.page(1)
 	except:
 		results = paginator_.page(paginator_.num_pages)
-	return render(request, 'polls/home.html', {'results' :results})
+	return render(request, 'polls/home.html', {'results' : results, 'url_string' : url_string})
